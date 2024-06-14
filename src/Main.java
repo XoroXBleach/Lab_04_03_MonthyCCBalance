0543de8a9e3528b26fd3d341d0aae465d22f2eda
xoroxbleach <morinxander04@gmail.com>
2024-06-13 23:39:23 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double CardBalance = 5000;
+        CardBalance = CardBalance * 1.17;
+        System.out.println("Your balance after one month is " + CardBalance + ".");
+        CardBalance = CardBalance * 1.17;
+        System.out.println("Your balance after two months is " + CardBalance + ".");
+        }
+    }
